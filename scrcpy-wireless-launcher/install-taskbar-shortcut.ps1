f1324b4080dfc2ef1edc2398328b677cac229b5a
stevensterkst <stevensterkst@gmail.com>
2026-09-21 18:44:16 +0200

    Improve taskbar shortcut icon selection

--- scrcpy-wireless-launcher/install-taskbar-shortcut.ps1
+++ scrcpy-wireless-launcher/install-taskbar-shortcut.ps1
@@ -20,7 +20,9 @@ $shortcut = $shell.CreateShortcut($shortcutPath)
 $shortcut.TargetPath = "$env:ComSpec"
 $shortcut.Arguments = '/c ""' + $LauncherPath + '""'
 $shortcut.WorkingDirectory = Split-Path -Parent $LauncherPath
-$shortcut.IconLocation = "$LauncherPath,0"
+$scrcpy = Join-Path $PSScriptRoot "scrcpy.exe"
+if (-not (Test-Path -LiteralPath $scrcpy)) { $scrcpy = "C:\Program Files\scrcpy-win64-v3.3.4\scrcpy.exe" }
+if (Test-Path -LiteralPath $scrcpy) { $shortcut.IconLocation = "$scrcpy,0" } else { $shortcut.IconLocation = "$env:SystemRoot\System32\shell32.dll,220" }
 $shortcut.Description = "Discover and launch connected Android phones through scrcpy"
 $shortcut.Save()
 
