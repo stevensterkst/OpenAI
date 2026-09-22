a76247e24264d9eaedd1cdc1fb964a49498c8553
stevensterkst <stevensterkst@gmail.com>
2026-09-22 10:46:37 +0200

    Remove obsolete Python setup and launcher scripts

--- transcribe-translate/setup.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0scriptssetup.ps1"
-if errorlevel 1 (echo SETUP FAILED.&pause&exit /b 1)
-echo.
-echo Setup complete. Run run.bat.
-pause
